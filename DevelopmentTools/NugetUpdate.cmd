39355b0cbed068539a1116afc6176c0e8793fa9c
James John McGuire <jamesjohnmcguire@gmail.com>
2023-01-10 04:41:09 +0900

    Improve Build Script

--- DevelopmentTools/NugetUpdate.cmd
+++ DevelopmentTools/NugetUpdate.cmd
@@ -4,8 +4,11 @@ REM %2 - API key
 CD %~dp0
 CD ..\DbxOutlookExpressLibrary
 
-msbuild -property:Configuration=Release -restore DbxOutlookExpressLibrary.csproj
-msbuild -property:Configuration=Release;OutputPath=Bin\Nuget DbxOutlookExpressLibrary.csproj
+msbuild -property:Configuration=Release;OutputPath=Bin\Nuget -restore DbxOutlookExpressLibrary.csproj
+
+if "%~2"=="" GOTO error1
+if "%~3"=="" GOTO error2
+
 CD Bin\Nuget
 
 dotnet nuget push DigitalZenWorks.Email.DbxOutlookExpress.%1.nupkg --api-key %2 --source https://api.nuget.org/v3/index.json
